79b2e55604e1e7d3cd9806d61cb35418b06c13bd
Benjamin Amaro <benjamin.amaro.sbam@gmail.com>
2023-11-06 22:42:33 -0500

    api

--- lesson05/scripts/weather.js
+++ lesson05/scripts/weather.js
@@ -3,3 +3,4 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
+
