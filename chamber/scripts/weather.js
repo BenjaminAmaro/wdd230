86b0c243ab85aa915d7a27b7b86f183298c97022
Benjamin Amaro <benjamin.amaro.sbam@gmail.com>
2023-11-22 22:00:08 -0500

    fixes Part 1 W11

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,5 +1,5 @@
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
+const weatherContainer = document.querySelector('#weather-container');
 const captionDesc = document.querySelector('figcaption');
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=46.75&lon=-71.24&units=imperial&appid=1961a74e36813368e84efb25d512b68d';
 
@@ -7,8 +7,8 @@ async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
-        const data = await response.json();
-        displayResults(data);
+            const data = await response.json();
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -21,15 +21,18 @@ apiFetch();
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
+
+    const weatherIcon = document.createElement('img');
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    weatherIcon.setAttribute('width', '30px');
-    weatherIcon.setAttribute('height', '30px');
+    weatherIcon.setAttribute('width', '50px');
+    weatherIcon.setAttribute('height', '50px');
+    weatherContainer.appendChild(weatherIcon);
     captionDesc.textContent = `${desc}`;
 }
-
 
 
 const url2 = 'https://api.openweathermap.org/data/2.5/forecast?lat=46.75&lon=-71.24&units=imperial&appid=33725e6989578392fee434ee33fa43f1'
